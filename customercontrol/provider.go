4ae24e8520f864a93ff90547d5333733f35c8a4b
Pavel Pikat <pavel.pikat@amcsgroup.com>
2021-05-15 21:06:04 +0200

    WIP

---
+++ customercontrol/provider.go
@@ -0,0 +1,57 @@
+package customercontrol
+
+import (
+	"context"
+
+	"dev.azure.com/amcsgroup/DevOps/_git/CustomerControlClientGo.git"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// Provider -
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"url": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: true,
+				DefaultFunc: schema.EnvDefaultFunc("CUSTOMERCONTROL_URL", "https://customercontrol-dev.amcsgroup.io"),
+			},
+			"privateKey": &schema.Schema{
+				Type: schema.TypeString,
+				Optional: false,
+			},
+		},
+		ResourcesMap:   map[string]*schema.Resource{},
+		DataSourcesMap: map[string]*schema.Resource{},
+	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	url := d.Get("url").(string)
+	privateKey := d.Get("privateKey").(string)
+
+	var diags diag.Diagnostics
+
+	if (url != "") && (privateKey != "") {
+		client, err := CustomerControlClientGo.NewClient(&url, &privateKey)
+
+		if err != nil {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary: "Unable to create CustomerControl client",
+			})
+
+			return nil, diags
+		}
+
+		return client, diags
+	} else {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary: "CustomerControl URL and privateKey must be provided",
+		})
+	}
+
+	return nil, diags
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module terraform-provider-customercontrol
+
+go 1.16
+
+require (
+	dev.azure.com/amcsgroup/DevOps/_git/CustomerControlClientGo.git v0.0.0-20210515184014-0fe6482ceb95 // indirect
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.6.1
+)
